401bd0ad31ac133d9e96387c17c9afeaaf126a47
JediLuke <1879634+JediLuke@users.noreply.github.com>
2024-10-03 23:04:28 -0500

    aded tools

--- lib/widget_workbench/widget_wkb_scene.ex
+++ lib/widget_workbench/widget_wkb_scene.ex
@@ -4,7 +4,9 @@ defmodule WidgetWorkbench.Scene do
 
   alias Scenic.Graph
   alias Scenic.Primitives
+  alias Scenic.Components
   alias Widgex.Frame
+  alias Scenic.ViewPort
 
   @moduledoc """
   A scene that serves as a widget workbench for designing and testing GUI components.
@@ -28,8 +30,8 @@ defmodule WidgetWorkbench.Scene do
       |> assign(frame: frame)
       |> push_graph(graph)
 
-    # Request input events if needed
-    # request_input(scene, [:cursor_pos, :cursor_button, :key])
+    # Request input events
+    request_input(scene, [:cursor_pos, :cursor_button, :key])
 
     {:ok, scene}
   end
@@ -50,10 +52,55 @@ defmodule WidgetWorkbench.Scene do
           text_align: :center,
           translate: {frame.size.width / 2, 50}
         )
+        # Add the tool palette
+        |> render_tool_palette(frame)
 
         # Add more components or widgets here
       end,
       translate: frame.pin.point
+    )
+  end
+
+  # Function to render the tool palette
+  defp render_tool_palette(graph, %Frame{} = frame) do
+    palette_width = 200
+    palette_height = 100
+    # Position on the right with a 20-pixel margin
+    palette_x = frame.size.width - palette_width - 20
+    # Position below the title text
+    palette_y = 70
+
+    # Draw the tool palette
+    graph
+    |> Primitives.group(
+      fn graph ->
+        graph
+        # Draw the rounded rectangle background
+        |> Primitives.rounded_rectangle(
+          {palette_width, palette_height, 10},
+          fill: :light_gray,
+          stroke: {1, :dark_gray},
+          translate: {0, 0}
+        )
+        # Add the "New Widget" button
+        |> Components.button(
+          "New Widget",
+          id: :new_widget_button,
+          width: palette_width - 20,
+          height: 30,
+          translate: {10, 10}
+        )
+        # Add the "Close Workbench" button
+        |> Components.button(
+          "Close Workbench",
+          id: :close_workbench_button,
+          width: palette_width - 20,
+          height: 30,
+          translate: {10, 50}
+        )
+      end,
+      id: :tool_palette,
+      translate: {palette_x, palette_y}
     )
   end
 
@@ -63,4 +110,21 @@ defmodule WidgetWorkbench.Scene do
     Logger.debug("Widget Workbench received input: #{inspect(input)}")
     {:noreply, scene}
   end
+
+  @impl Scenic.Scene
+  def handle_event({:click, :new_widget_button}, _from, scene) do
+    Logger.info("New Widget button clicked!")
+    # Implement the action for creating a new widget here
+    {:noreply, scene}
+  end
+
+  def handle_event({:click, :close_workbench_button}, _from, scene) do
+    Logger.info("Close Workbench button clicked!")
+    # Implement the action for closing the workbench
+    # For example, switch back to the root scene:
+    # {:ok, _} = ViewPort.set_root(scene.viewport, {Flamelex.GUI.RootScene, nil})
+    {:noreply, scene}
+  end
+
+  def handle_event(_event, _from, scene), do: {:noreply, scene}
 end
