b210dd168edb007a71925f3efcdb3ec529fb4c9d
Luke JT <luke@Leahs-Air.lan>
2025-04-13 20:15:46 -0700

    update scenic 0.11.1

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule ScenicWidgets.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:scenic, "~> 0.11.0-beta.0"},
+      {:scenic, "~> 0.11.1"},
       # {:scenic, git: "https://github.com/JediLuke/scenic", branch: "no_text_wrap"},
       {:font_metrics, "~> 0.5"},
       {:ex_doc, "~> 0.25", only: :dev},
