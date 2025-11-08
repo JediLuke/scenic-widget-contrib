e0c0c4cde96f543878f588d6213c57fbbcb2ea54
Jason Axelson <jason.axelson@gmail.com>
2025-11-07 23:53:13 -0600

    WIP

--- mix.exs
+++ mix.exs
@@ -26,18 +26,20 @@ defmodule ScenicWidgets.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:scenic, git: "https://github.com/ScenicFramework/scenic.git", tag: "v0.11.1", override: true},
+      # {:scenic, git: "https://github.com/ScenicFramework/scenic.git", tag: "v0.11.1", override: true},
+      {:scenic, path: "../scenic", override: true},
       {:scenic_driver_local, git: "https://github.com/JediLuke/scenic_driver_local", branch: "flamelex_vsn", override: true},
       {:font_metrics, "~> 0.5"},
       {:ex_doc, "~> 0.25", only: :dev},
       {:earmark, "~> 1.4", only: :dev},
-      {:stream_data, "~> 0.5", only: :test},
+      # {:stream_data, "~> 1.0", only: :test},
       # Added dependencies for widget development
       {:scenic_mcp, path: "../scenic_mcp", optional: true},
       {:sexy_spex, path: "../spex", optional: true},
       {:scenic_live_reload, git: "https://github.com/axelson/scenic_live_reload.git", branch: "main", only: :dev},
       {:tidewave, "~> 0.1", only: :dev},
-      {:bandit, "~> 1.0", only: :dev}
+      {:bandit, "~> 1.0", only: :dev},
+      {:pomodoro, path: "~/dev/pomodoro"}
     ]
   end
 
